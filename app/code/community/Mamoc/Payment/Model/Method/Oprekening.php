eefc2bcf0efb3dc047a5f5c02120e9f145b913d6
Vladimir <Vladimir@10.10.20.75>
2012-07-13 10:02:07 +0200

    + Generieke code naar Abstract class overgezet

--- app/code/community/Mamoc/Payment/Model/Method/Oprekening.php
+++ app/code/community/Mamoc/Payment/Model/Method/Oprekening.php
@@ -5,31 +5,10 @@
  * @copyright   Copyright (c) 2012 MaMoC BV (http://www.mamoc.net)
  */
 
-class Mamoc_Payment_Model_Method_Oprekening extends Mage_Payment_Model_Method_Abstract {
+class Mamoc_Payment_Model_Method_Oprekening extends Mamoc_Payment_Model_Method_Abstract {
 
     protected $_code  = 'oprekening';
 
 	protected $_canCapture    = true;
-
-	public function isAvailable($quote = null) {
-		$allowed_groups		= explode(',',Mage::getStoreConfig('payment/'.$this->_code.'/allow_group'));
-		$min_order_amount	= Mage::getStoreConfig('payment/'.$this->_code.'/min_order_amount');
-		$max_order_amount	= Mage::getStoreConfig('payment/'.$this->_code.'/max_order_amount');
-
-		if( (Mage::app()->getStore()->roundPrice($quote->getGrandTotal())) < (Mage::app()->getStore()->roundPrice($min_order_amount)) ) {
-			return false;
-		}
-
-		if( (Mage::app()->getStore()->roundPrice($quote->getGrandTotal())) >= (Mage::app()->getStore()->roundPrice($max_order_amount)) ) {
-			return false;
-		}
-
-		if(!in_array($quote->getCustomerGroupId(),$allowed_groups)) {
-			return false;
-		}
-
-        return parent::isAvailable($quote);
-    }
-
 
 }
