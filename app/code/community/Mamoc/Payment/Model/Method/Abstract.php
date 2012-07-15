716941227f076ea9c458b6d27eeb9b01cd1d0dd7
Vladimir <Vladimir@10.10.20.75>
2012-07-15 21:25:53 +0200

    + Check of min en max order waarde zijn ingevuld + modman file toegevoegd.

--- app/code/community/Mamoc/Payment/Model/Method/Abstract.php
+++ app/code/community/Mamoc/Payment/Model/Method/Abstract.php
@@ -10,13 +10,20 @@ class Mamoc_Payment_Model_Method_Abstract extends Mage_Payment_Model_Method_Abst
 	public function isAvailable($quote = null) {
 		$allowed_groups		= explode(',',Mage::getStoreConfig('payment/'.$this->_code.'/allow_group'));
 		$min_order_amount	= Mage::getStoreConfig('payment/'.$this->_code.'/min_order_amount');
+		if(!is_numeric($min_order_amount)){
+			$min_order_amount = 0;
+		}
 		$max_order_amount	= Mage::getStoreConfig('payment/'.$this->_code.'/max_order_amount');
+		if(!is_numeric($max_order_amount)){
+			$max_order_amount = 0;
+		}
 
-		if( (Mage::app()->getStore()->roundPrice($quote->getGrandTotal())) < (Mage::app()->getStore()->roundPrice($min_order_amount)) ) {
+
+		if( $min_order_amount>0 && (Mage::app()->getStore()->roundPrice($quote->getGrandTotal()) < Mage::app()->getStore()->roundPrice($min_order_amount)) ) {
 			return false;
 		}
 
-		if( (Mage::app()->getStore()->roundPrice($quote->getGrandTotal())) >= (Mage::app()->getStore()->roundPrice($max_order_amount)) ) {
+		if($max_order_amoun>0 && (Mage::app()->getStore()->roundPrice($quote->getGrandTotal()) >= Mage::app()->getStore()->roundPrice($max_order_amount)) ) {
 			return false;
 		}
 
